4e7dc32973fb109f8b24696002427ecdec1ffd59
ronaldpiedra <ra.piedraz@uea.edu.ec>
2024-09-09 22:08:07 -0500

    Add files via upload

---
+++ Deber_s11_2_rp.py
@@ -0,0 +1,10 @@
+#Deber semana 11 Ronald Piedra
+columna_a =[10, 20, 30]
+columna_b =[30, 40, 50]
+columna_c =[50, 60, 70]
+
+matriz = [columna_a, columna_b, columna_c] #3x3
+
+print(matriz[0][2])
+print(matriz[2][0])
+print(matriz[2][2])
