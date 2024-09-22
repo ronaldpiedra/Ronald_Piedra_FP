7ad8535b093abfc8ee82b8f421a785d25754ec99
ronaldpiedra <ra.piedraz@uea.edu.ec>
2024-09-22 13:05:47 -0500

    Create Deber_s14_rp.py

---
+++ Deber_s14_rp.py
@@ -0,0 +1,17 @@
+def saludar(nombre, edad):
+    print(f"hola, {nombre} tienes {edad} años.")
+
+
+saludar("ronald", 33)
+saludar("Isabel", 32)
+
+
+def saludar_con_saludo(nombre, edad, saludo="hola"):
+    print(f"{saludo}, {nombre} tienes {edad} años.")
+
+
+saludar_con_saludo("ronald", 33)
+saludar_con_saludo("Isabel", 32, "hola")
+
+
+
